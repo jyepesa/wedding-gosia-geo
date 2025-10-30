137287add90ca2be7ed9843778b39247d3a761a6
jyepesa <juank_713@hotmail.com>
2025-10-30 22:04:06 +0100

    setup project

---
+++ src/i18n/de.js
@@ -0,0 +1,46 @@
+export const de = {
+  meta: {
+    lang: "de",
+    title: "Hochzeit Gosia und Geo",
+  },
+  heading: {
+    title: "Hochzeitszeitung",
+    "date-city": {
+      date: "26 juni 2026",
+      city: "Warschau",
+    },
+    type: "Unsere große polnisch-griechische Hochzeit",
+    announcement: "Małgorzata & Georgios <span>heiraten!</span>",
+  },
+  invitation: {
+    title: "Liebe Familie und Freunde,",
+    message:
+      "Mit Freude möchten wir euch zu unserer Hochzeitsfeier und der anschließenden Hochzeitsparty am <strong>26.06.2026</strong> einladen. <br>An diesem besonderen Tag würden wir uns geehrt fühlen, wenn ihr uns mit eurer Anwesenheit beehren würdet.",
+  },
+  timetable: {
+    title: "Tagesablauf",
+    first:
+      "<span>15:30</span> - Trauung im Garten des Gasthofs „Oycowizna”, <br> Empfang mit Champagner und Fotos",
+    second:
+      "<span>17:00</span> - Festliches Hochzeitsessen und Beginn der Feier im Gasthof „Oycowizna“",
+  },
+  important: {
+    title: "Es lohnt sich, sie zu kennen",
+    telephones: [
+      "Joanna - Trauzeugin, nr. tel.",
+      "Juan- Trauzeuge, nr. tel.",
+      "Gosia - nr. tel…………..",
+      "Georgios - nr. tel..",
+    ],
+  },
+  confirmation:
+    "<strong>Wir bitten herzlich um Ihre Zusage</strong> bis spätestens <strong>01.04.2026</strong>, idealerweise per Nachricht an die Trauzeugen oder direkt an uns.",
+  accomodation:
+    "<i>Wegen der Unterkunft</i> - bitte gebt unseren Trauzeugen (Joanna & Juan) Bescheid, wer von Euch eine Übernachtung benötigt, damit wir dies an die entsprechenden Personen weitergeben können. Die Übernachtung kostet ca. 35-40 € pro Person.",
+  presents:
+    "Wir wissen, dass die Reise nach Polen für das Hochzeitsfest eine gewisse Ausgabe bedeutet, und Ihre Anwesenheit ist für uns das schönste Geschenk! Falls jedoch jemand den Wunsch hat, uns etwas zu schenken, wären wir sehr dankbar für",
+  story: {
+    title: "Unsere <icon> geschichte",
+    text: "Wie ein polnisches Mädchen einen griechischen Jungen kennenlernte… <br> Es war ein Junimorgen im Jahr 2020, als die Welt nach Kaffee und Unsicherheit duftete. <br>In einem kleinen Café, in dem das Aroma von Espresso wie ein Zauber in der Luft hing, verzauberte Małgorzata die Menschen mit einem Lächeln hinter der Maske und Augen so blau, dass selbst der Himmel neidisch wurde. <br>Eines Tages öffnete sich die Tür - und herein kam Georgios, ein Elektriker von der Baustelle nebenan. <br>Ein Mann, der Strom bändigen konnte, aber nicht darauf vorbereitet war, gleich selbst einen Schlag zu bekommen - vom Blitz der Liebe. Denn als er Małgorzata in die Augen sah, verstand er: Dieses Funkeln hatte mehr Spannung als alle Kabel, die er je angeschlossen hatte. <br>Von da an kam Georgios dreimal während einer Schicht vorbei - einmal für einen Kaffee, einmal für ein Croissant und einmal… einfach so, um sie wiederzusehen. <br>Und Małgorzata, die das Spiel längst durchschaut hatte, machte ihm den Kaffee jedes Mal ein bisschen stärker, nur um das Gespräch noch ein wenig zu verlängern. <br>Eines Tages drückte sie den Knopf der Maschine so fest, dass aus einem Espresso plötzlich zehn wurden - und obwohl Georgios' Herz fast stehen blieb, wussten beide, dass es nicht an der Koffeindosis lag, sondern an der Liebe. <br>So begann ihre Geschichte - aus einem zufälligen Blick, dem Duft von Kaffee und Funken, die ganz ohne Strom flogen. <br>Heute stehen Małgorzata und Georgios Seite an Seite, bereit, ihr gemeinsames Leben zu „brühen“: heiß, aromatisch, mit einer Prise Verrücktheit und einer großen Portion Liebe.",
+  },
+};
---
+++ src/i18n/pl.js
@@ -0,0 +1,44 @@
+export const pl = {
+  meta: {
+    lang: "pl",
+    title: "Ślub Gosi!",
+  },
+  heading: {
+    title: "Gazeta Ślubna",
+    "date-city": {
+      date: "26 czerwca 2026",
+      city: "Warszawa",
+    },
+    type: "Nasze wielkie polsko-greckie wesele",
+    announcement: "Małgorzata & Georgios <span>biorą ślub !</span>",
+  },
+  invitation: {
+    title: "Droga Rodzino i Przyjaciele,",
+    message:
+      "Z radością chcielibyśmy Was zaprosić na uroczystość zaślubin i wesele, <span>w dniu <strong>26.06.2026.</strong></span>W tym wyjątkowym dniu będziemy zaszczyceni, jeśli obdarzycie nas swojąobecnością.",
+  },
+  timetable: {
+    title: "Harmonogram dnia",
+    first:
+      "<span>15:30</span> - ceremonia zaslubin w ogrodzie Goscinca „Oycowizna“,<br>powitalny szampan i zdjecia",
+    second:
+      "<span>17:00</span> - uroczysty obiad weselny i rozpoczecie zabawy w Goscincu „Oycowizna“",
+  },
+  important: {
+    title: "Warto ich znac",
+    telephones: [
+      "Asia- swiadkowa, nr. tel. ……",
+      "Juan- swiadek, nr. tel. ………….",
+      "Gosia- nr. tel…………..",
+      "Georgios- nr. tel",
+    ],
+  },
+  confirmation:
+    "<strong>Bardzo prosimy o potwierdzenie przybycia</strong> do dnia <strong>01.04.2026</strong>, najlepiej wiadomością do świadków lub bezpośrednio do nas.",
+  presents:
+    "<strong>Wasza obecność to dla nas najpiękniejszy prezent!</strong> Jeślibyscie chcieli nas obdarować:",
+  story: {
+    title: "Nasza <icon> historia",
+    text: "Jak polska dziewczyna poznała greckiego chłopaka… <br> Był czerwcowy poranek roku 2020, gdy świat pachniał kawą i niepewnością. <br> W małej kawiarni, gdzie aromat espresso unosił się jak zaklęcie, Małgorzata czarowała ludzi uśmiechem ukrytym za maseczką i oczami tak błękitnymi, że potrafiły rozświetlić nawet najbardziej pochmurny dzień. <br> Pewnego dnia drzwi otworzyły się i wszedł Georgios - elektryk z sąsiedniej budowy, człowiek, który potrafił ujarzmić prąd, ale nie był gotowy na to, co zaraz go porazi. <br> Bo gdy spojrzał w oczy Małgorzaty, zrozumiał, że to spojrzenie ma większe napięcie niż wszystkie kable, które kiedykolwiek podłączał. Od tego dnia Georgios zaczął przychodzić trzy razy podczas jednej zmiany. Raz po kawę, raz po croissanta, a raz… po pretekst, by znów zobaczyć jej oczy. <br> A Małgorzata, wiedząc doskonale, co się święci, parzyła mu kawy coraz mocniejsze, przedłużając każdą rozmowę i każdą sekundę. Kiedyś włączyła ekspres tak, że zamiast jednej kawy wyszło dziesięć espresso — i choć serce Georgiosa prawie stanęło, oboje wiedzą, że to nie z powodu kofeiny, tylko od miłości. <br>Tak narodziła się ich historia — z przypadkowego spojrzenia, z aromatu kawy i iskier, które wcale nie potrzebowały prądu. <br>Dziś Małgorzata i Georgios stoją razem, gotowi zaparzyć wspólne życie: gorące, aromatyczne, z odrobiną szaleństwa i dużą dawką miłości.",
+  },
+};
